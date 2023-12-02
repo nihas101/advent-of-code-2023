234cff7fde379fa5c8d7cc4dd377cec16b7c0fd3
Nikita Hasert <nikitahasert@gmail.com>
2023-12-02 08:35:21 +0100

    Day 02: Solve part 1 and 2

--- src/advent_of_code_2023/day02.clj
+++ src/advent_of_code_2023/day02.clj
@@ -2,8 +2,52 @@
   (:require
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn- parse-die [die]
+  (let [[[num col]] (u/split-pairs die)]
+    {col (Long/parseLong num)}))
 
-(defn day02-1 [parsed-input])
+(defn- parse-game [[game & dice]]
+  [(Long/parseLong (re-find #"\d+" game))
+   (transduce
+    (comp
+     (map #(u/split-sections % #"\s*,\s*"))
+     (map (fn [d] (transduce (map parse-die) merge d))))
+    conj [] dice)])
 
-(defn day02-2 [parsed-input])
+(defn parse-input [games]
+  (transduce
+   (comp
+    (map #(u/split-sections % #"\s*:\s*|\s*;\s*"))
+    (remove (comp empty? first))
+    (map parse-game))
+   conj [] (u/split-sections games u/line-endings)))
+
+(defonce ^:private bag
+  {"red" 12
+   "green" 13
+   "blue" 14})
+
+(defn- max-dice [[game dice]]
+  [game (apply merge-with max dice)])
+
+(defn- possible-game-dice? [game-dice]
+  (fn [[col ^long max-dice]] (<= ^long (game-dice col 0) max-dice)))
+
+(defn day02-1 [parsed-input]
+  (transduce
+   (comp
+    (map max-dice)
+    (filter (comp #(every? (possible-game-dice? %) bag) second))
+    (map first))
+   + parsed-input))
+
+(defn- dice-power [dice]
+  (reduce * (vals dice)))
+
+(defn day02-2 [parsed-input]
+  (transduce
+   (comp
+    (map max-dice)
+    (map second)
+    (map dice-power))
+   + parsed-input))
--- test/advent_of_code_2023/day02_test.clj
+++ test/advent_of_code_2023/day02_test.clj
@@ -3,26 +3,26 @@
    [clojure.test :refer :all]
    [advent-of-code-2023.day02 :refer :all]))
 
-(def ^:private example-input (parse-input ""))
+(def ^:private example-input (parse-input "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"))
 
 (def ^:private input (parse-input (slurp "resources/day02.txt")))
 
 (deftest day02-1-example-test
   (testing "day02-1 example"
-    (is (= nil
-           (day02-1 example-input)))))
+    (is (= 8 (day02-1 example-input)))))
 
 (deftest day02-1-test
   (testing "day02-1"
-    (is (= nil
-           (day02-1 input)))))
+    (is (= 2727 (day02-1 input)))))
 
 (deftest day02-2-example-test
   (testing "day02-2 example"
-    (is (= nil
-           (day02-2 example-input)))))
+    (is (= 2286 (day02-2 example-input)))))
 
 (deftest day02-2-test
   (testing "day02-2"
-    (is (= nil
-           (day02-2 input)))))
+    (is (= 56580 (day02-2 input)))))
