fe28d156ccf840ebc614d8d8eac23139dc1cb659
Nikita Hasert <nikitahasert@gmail.com>
2023-12-03 11:15:28 +0100

    Day 03: Sovle part 1 and 2

--- src/advent_of_code_2023/day03.clj
+++ src/advent_of_code_2023/day03.clj
@@ -2,8 +2,63 @@
   (:require
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn- match-line-elements [^java.util.regex.Matcher matcher graph-elements element-parser y]
+  (if-let [match (re-find matcher)]
+    (recur matcher
+           (reduce (fn [r x]
+                     ;; We need to identify duplicate gear parts later on,
+                     ;; so we uniquely identify them via their starting position
+                     (assoc r [x y] [[(.start matcher) y] (element-parser match)]))
+                   graph-elements (range (.start matcher) (.end matcher)))
+           element-parser
+           y)
+    graph-elements))
 
-(defn day03-1 [parsed-input])
+(defn- parse-line-elements-fn [regex element-parser]
+  (fn [res [line y]]
+    (match-line-elements (re-matcher regex line) res element-parser y)))
 
-(defn day03-2 [parsed-input])
+(defn parse-input [input]
+  (let [indexed-lines (mapv vector
+                            (u/split-sections input u/line-endings)
+                            (range))]
+    {:parts (reduce (parse-line-elements-fn #"\d+" #(Long/parseLong %)) {} indexed-lines)
+     :symbols (reduce (parse-line-elements-fn #"[^\d\.]" identity) {} indexed-lines)}))
+
+(defn- neighbours [[^long x ^long y]]
+  (for [xx (range (dec x) (+ x 2))
+        yy (range (dec y) (+ y 2))
+        :when (or (not= xx x) (not= yy y))]
+    [xx yy]))
+
+(defn day03-1 [parsed-input]
+  (transduce
+   (comp
+    (mapcat neighbours)
+    (map (:parts parsed-input))
+    (remove nil?)
+    (distinct)
+    (map second))
+   + (keys (:symbols parsed-input))))
+
+(defn- gear-ratio-fn [parts]
+  (fn gear-ratio [ns]
+    (let [[^long a ^long b :as adjacent-numbers] (transduce
+                                                  (comp
+                                                   (map parts)
+                                                   (remove nil?)
+                                                   (distinct)
+                                                   (map second))
+                                                  conj [] ns)]
+      (if (= 2 (count adjacent-numbers))
+        (* a b)
+        0))))
+
+(defn day03-2 [parsed-input]
+  (transduce
+   (comp
+    (filter (comp #{"*"} second second))
+    (map (comp first second))
+    (map neighbours)
+    (map (gear-ratio-fn (:parts parsed-input))))
+   + (:symbols parsed-input)))
--- test/advent_of_code_2023/day03_test.clj
+++ test/advent_of_code_2023/day03_test.clj
@@ -3,26 +3,31 @@
    [clojure.test :refer :all]
    [advent-of-code-2023.day03 :refer :all]))
 
-(def ^:private example-input (parse-input ""))
+(def ^:private example-input (parse-input "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."))
 
 (def ^:private input (parse-input (slurp "resources/day03.txt")))
 
 (deftest day03-1-example-test
   (testing "day03-1 example"
-    (is (= nil
-           (day03-1 example-input)))))
+    (is (= 4361 (day03-1 example-input)))))
 
 (deftest day03-1-test
   (testing "day03-1"
-    (is (= nil
-           (day03-1 input)))))
+    (is (= 532331 (day03-1 input)))))
 
 (deftest day03-2-example-test
   (testing "day03-2 example"
-    (is (= nil
-           (day03-2 example-input)))))
+    (is (= 467835 (day03-2 example-input)))))
 
 (deftest day03-2-test
   (testing "day03-2"
-    (is (= nil
-           (day03-2 input)))))
+    (is (= 82301120 (day03-2 input)))))
