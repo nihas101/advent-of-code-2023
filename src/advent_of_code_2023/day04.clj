291235376ce70b5ac5368463d88bce9f8fa9201f
Nikita Hasert <nikitahasert@gmail.com>
2023-12-04 09:59:25 +0100

    Day 04: Solve part 1 and part 2

--- src/advent_of_code_2023/day04.clj
+++ src/advent_of_code_2023/day04.clj
@@ -1,9 +1,54 @@
 (ns advent-of-code-2023.day04
   (:require
+   [clojure.math :as m]
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn parse-input [input]
+  (transduce
+   (comp
+    (map #(u/split-sections % #":\s+"))
+    (map second)
+    (remove nil?)
+    (map (fn [numbers] (u/split-sections numbers #"\s+\|\s+")))
+    (map-indexed (fn [idx [winning-numbers numbers]]
+                   [idx
+                    (set (u/read-longs winning-numbers #"\s+"))
+                    (u/read-longs numbers #"\s+")])))
+   conj [] (u/split-sections input u/line-endings)))
 
-(defn day04-1 [parsed-input])
+(defn day04-1 [scratch-cards]
+  (transduce
+   (comp
+    (map rest)
+    (map (fn [[winning-numbers numbers]] (filterv winning-numbers numbers)))
+    (map (fn [numbers]
+           (if (pos? (count numbers)) (long (m/pow 2 (dec (count numbers)))) 0))))
+   + scratch-cards))
 
-(defn day04-2 [parsed-input])
+
+(defn- won-copy-indexes [scratch-cards]
+  (fn [[^long idx & sc]]
+    (let [sc-numbers (count ((fn [[winning-numbers numbers]]
+                               (filterv winning-numbers numbers)) sc))
+          start (inc idx)]
+      (if (zero? sc-numbers)
+        [idx []]
+        [idx (range (inc idx) (min (+ start sc-numbers)
+                                   (count scratch-cards)))]))))
+
+(defn- tally-scratch-cards
+  ([scc] scc)
+  ([sc-count [idx r]]
+   (reduce (fn [scc ^long i]
+             (if (< (count scc) i)
+               scc
+               (update scc i + (sc-count idx))))
+           sc-count r)))
+
+(defn day04-2 [scratch-cards]
+  (->> scratch-cards
+       (transduce
+        (map (won-copy-indexes scratch-cards))
+        tally-scratch-cards
+        (vec (repeat (count scratch-cards) 1)))
+       (reduce +)))
--- test/advent_of_code_2023/day04_test.clj
+++ test/advent_of_code_2023/day04_test.clj
@@ -3,26 +3,27 @@
    [clojure.test :refer :all]
    [advent-of-code-2023.day04 :refer :all]))
 
-(def ^:private example-input (parse-input ""))
+(def ^:private example-input (parse-input "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"))
 
 (def ^:private input (parse-input (slurp "resources/day04.txt")))
 
 (deftest day04-1-example-test
   (testing "day04-1 example"
-    (is (= nil
-           (day04-1 example-input)))))
+    (is (= 13 (day04-1 example-input)))))
 
 (deftest day04-1-test
   (testing "day04-1"
-    (is (= nil
-           (day04-1 input)))))
+    (is (= 25231 (day04-1 input)))))
 
 (deftest day04-2-example-test
   (testing "day04-2 example"
-    (is (= nil
-           (day04-2 example-input)))))
+    (is (= 30 (day04-2 example-input)))))
 
 (deftest day04-2-test
   (testing "day04-2"
-    (is (= nil
-           (day04-2 input)))))
+    (is (= 9721255 (day04-2 input)))))
