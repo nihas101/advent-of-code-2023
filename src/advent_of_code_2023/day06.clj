d17639bd82a3bd43226d95dcf70cc6ae5236403e
Nikita Hasert <nikitahasert@gmail.com>
2023-12-06 09:19:19 +0100

    Day 06: Solve part 1 and 2

--- src/advent_of_code_2023/day06.clj
+++ src/advent_of_code_2023/day06.clj
@@ -1,9 +1,45 @@
 (ns advent-of-code-2023.day06
   (:require
+   [clojure.math :as math]
+   [clojure.string :as string]
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn parse-input [input prep-fun]
+  (let [[time distance] (mapv (fn [s] (re-seq #"\d+" (prep-fun s)))
+                              (u/split-sections input u/line-endings))]
+    (mapv (fn [t d] {:duration t
+                     :record d})
+          (mapv #(Long/parseLong %) time)
+          (mapv #(Long/parseLong %) distance))))
 
-(defn day06-1 [parsed-input])
+(defn- error-margin-brute-force [{:keys [^long duration ^long record]}]
+  (loop [button-hold-time 0
+         ways-to-beat-the-record []]
+    (let [boat-distance (* button-hold-time (- duration button-hold-time))]
+      (cond
+        ;; We have checked every amount of time possible
+        (<= duration button-hold-time) ways-to-beat-the-record
+        ;; We beat the record
+        (< record (* button-hold-time (- duration button-hold-time)))
+        (recur (inc button-hold-time)
+               (conj ways-to-beat-the-record {:hold-time button-hold-time :distance boat-distance}))
+        :else (recur (inc button-hold-time)
+                     ways-to-beat-the-record)))))
 
-(defn day06-2 [parsed-input])
+(defn day06-1 [input]
+  (transduce
+   (comp
+    (map error-margin-brute-force)
+    (map count))
+   * (parse-input input identity)))
+
+(defn- error-margin [{:keys [^long duration ^long record]}]
+  (let [d (math/sqrt (- (* duration duration) (* 4 (- 1) (- (inc record)))))]
+    [(long (quot (+ (- duration) d) (* 2 (- 1))))
+     (long (quot (- (- duration) d) (* 2 (- 1))))]))
+
+(defn day06-2 [input]
+  (let [[^long a ^long b] (error-margin
+                           (first
+                            (parse-input input #(string/replace % #"\s+" ""))))]
+    (- b a)))
--- test/advent_of_code_2023/day06_test.clj
+++ test/advent_of_code_2023/day06_test.clj
@@ -4,26 +4,23 @@
    [advent-of-code-2023.day06 :refer :all]
    [advent-of-code-2023.test-utils :as tu]))
 
-(def ^:private example-input "")
+(def ^:private example-input "Time:      7  15   30
+Distance:  9  40  200")
 
 (def ^:private input (tu/slurp-input "resources/day06.txt"))
 
 (deftest day06-1-example-test
   (testing "day06-1 example"
-    (is (= nil
-           (day06-1 example-input)))))
+    (is (= 288 (day06-1 example-input)))))
 
 (deftest day06-1-test
   (testing "day06-1"
-    (is (= nil
-           (day06-1 input)))))
+    (is (= 633080 (day06-1 input)))))
 
 (deftest day06-2-example-test
   (testing "day06-2 example"
-    (is (= nil
-           (day06-2 example-input)))))
+    (is (= 71503 (day06-2 example-input)))))
 
 (deftest day06-2-test
   (testing "day06-2"
-    (is (= nil
-           (day06-2 input)))))
+    (is (= 20048741 (day06-2 input)))))
