31237a05d417fcbc7738759e46cb89c7013480c9
Nikita Hasert <nikitahasert@gmail.com>
2023-12-08 07:46:37 +0100

    Day 08: Solve part 1 and 2

--- src/advent_of_code_2023/day08.clj
+++ src/advent_of_code_2023/day08.clj
@@ -1,9 +1,44 @@
 (ns advent-of-code-2023.day08
   (:require
+   [clojure.string :as string]
+   [advent-of-code-2023.utils.math :as math]
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn- parse-instructions [instr]
+  (mapv {\L :left \R :right} instr))
 
-(defn day08-1 [parsed-input])
+(defn- parse-map [m]
+  (transduce
+   (map (fn [line]
+          (let [[source left right] (re-seq #"[a-zA-Z0-9]{3}" line)]
+            {source {:left left
+                     :right right}})))
+   merge (u/split-sections m u/line-endings)))
 
-(defn day08-2 [parsed-input])
+(defn parse-input [input]
+  (when (seq input)
+    (let [[instr m] (u/split-sections input)]
+      {:instructions (parse-instructions instr)
+       :graph (parse-map m)})))
+
+(defn- steps-until-goal [instructions graph start goals]
+  (reduce (fn [[^long step loc] instr]
+            (let [next-loc ((graph loc) instr)]
+              (if (contains? goals next-loc)
+                (reduced (inc step))
+                [(inc step) next-loc])))
+          [0 start] (cycle instructions)))
+
+(defn day08-1 [{:keys [instructions graph]}]
+  (steps-until-goal instructions graph "AAA" #{"ZZZ"}))
+
+(defn- filter-keys [graph s]
+  (filterv (fn [source] (string/ends-with? source s))
+           (keys graph)))
+
+(defn day08-2 [{:keys [instructions graph]}]
+  (let [starts (filter-keys graph "A")
+        goals (set (filter-keys graph "Z"))]
+    (transduce
+     (map (fn [start] (steps-until-goal instructions graph start goals)))
+     math/lcm starts)))
--- test/advent_of_code_2023/day08_test.clj
+++ test/advent_of_code_2023/day08_test.clj
@@ -4,26 +4,51 @@
    [advent-of-code-2023.day08 :refer :all]
    [advent-of-code-2023.test-utils :as tu]))
 
-(def ^:private example-input (parse-input ""))
+(def ^:private example-input (parse-input "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"))
+
+(def ^:private example-input-2 (parse-input "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"))
 
 (def ^:private input (parse-input (tu/slurp-input "resources/day08.txt")))
 
 (deftest day08-1-example-test
-  (testing "day08-1 example"
-    (is (= nil
-           (day08-1 example-input)))))
+  (testing "day08-1 example 1"
+    (is (= 2 (day08-1 example-input)))))
+
+(deftest day08-1-example-2-test
+  (testing "day08-1 example 2"
+    (is (= 6 (day08-1 example-input-2)))))
 
 (deftest day08-1-test
   (testing "day08-1"
-    (is (= nil
-           (day08-1 input)))))
+    (is (= 13019 (day08-1 input)))))
 
-(deftest day08-2-example-test
-  (testing "day08-2 example"
-    (is (= nil
-           (day08-2 example-input)))))
+(def ^:private example-input-3 (parse-input "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"))
+
+(deftest day08-2-example-3-test
+  (testing "day08-2 example 3"
+    (is (= 6 (day08-2 example-input-3)))))
 
 (deftest day08-2-test
   (testing "day08-2"
-    (is (= nil
-           (day08-2 input)))))
+    (is (= 13524038372771 (day08-2 input)))))
