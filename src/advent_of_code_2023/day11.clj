74b1dbefb9618c8b525a2ce3f493c9f034f42593
Nikita Hasert <nikitahasert@gmail.com>
2023-12-11 20:12:50 +0100

    Day 11: Solve part 1 and 2

--- src/advent_of_code_2023/day11.clj
+++ src/advent_of_code_2023/day11.clj
@@ -1,9 +1,48 @@
 (ns advent-of-code-2023.day11
   (:require
-   [advent-of-code-2023.utils.string :as u]))
+   [advent-of-code-2023.utils.graph :as g]
+   [advent-of-code-2023.utils.math :as math]))
 
-(defn parse-input [input])
+(defn parse-input [input]
+  (let [{:keys [positions width height] :as m} (g/parse-positional-map input identity)
+        galaxy-positions (transduce (comp (filter (comp #{\#} second)) (map first))
+                                    conj [] positions)]
+    (-> m
+        (assoc ,,, :galaxies galaxy-positions)
+        (assoc ,,, :empty-cols (remove (transduce (map first)
+                                                  conj #{} galaxy-positions)
+                                       (range width)))
+        (assoc ,,, :empty-rows (remove (transduce (map second)
+                                                  conj #{} galaxy-positions)
+                                       (range height))))))
 
-(defn day11-1 [parsed-input])
+(defn- galaxy-distances [galaxies]
+  (for [[^long x ^long y :as g1] galaxies
+        [^long xx ^long yy :as g2] galaxies
+        :when (or (and (= y yy) (< x xx))
+                  (and (not= g1 g2) (< y yy)))]
+    {:a g1 :b g2
+     :distance (math/manhattan-distance g1 g2)}))
 
-(defn day11-2 [parsed-input])
+(defn- in-between [^long a ^long b]
+  (let [a' (min a b)
+        b' (max a b)]
+    (fn [^long x] (< a' x b'))))
+
+(defn- expand-distance-fn [empty-cols empty-rows ^long distance-factor]
+  (fn expand-distance [{[^long x ^long y] :a
+                        [^long xx ^long yy] :b
+                        :as galaxy-distance}]
+    (let [x-exp (count (filterv (in-between x xx) empty-cols))
+          y-exp (count (filterv (in-between y yy) empty-rows))]
+      (update galaxy-distance :distance +
+              (* distance-factor x-exp) (* distance-factor y-exp)))))
+
+(defn day11
+  ([galaxy-map] (day11 galaxy-map 2))
+  ([{:keys [galaxies empty-cols empty-rows]} ^long distance-factor]
+   (transduce
+    (comp
+     (map (expand-distance-fn empty-cols empty-rows (dec distance-factor)))
+     (map :distance))
+    + (galaxy-distances galaxies))))
--- test/advent_of_code_2023/day11_test.clj
+++ test/advent_of_code_2023/day11_test.clj
@@ -4,26 +4,35 @@
    [advent-of-code-2023.day11 :refer :all]
    [advent-of-code-2023.test-utils :as tu]))
 
-(defonce ^:private example-input (parse-input ""))
+(defonce ^:private example-input (parse-input "...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#....."))
 
 (def ^:private input (parse-input (tu/slurp-input "resources/day11.txt")))
 
 (deftest day11-1-example-test
   (testing "day11-1 example"
-    (is (= nil
-           (day11-1 example-input)))))
+    (is (= 374 (day11 example-input)))))
 
 (deftest day11-1-test
   (testing "day11-1"
-    (is (= nil
-           (day11-1 input)))))
+    (is (= 9556896 (day11 input)))))
 
-(deftest day11-2-example-test
-  (testing "day11-2 example"
-    (is (= nil
-           (day11-2 example-input)))))
+(deftest day11-2-example-10-test
+  (testing "day11-2 example 10"
+    (is (= 1030 (day11 example-input 10)))))
+
+(deftest day11-2-example-100-test
+  (testing "day11-2 example 100"
+    (is (= 8410 (day11 example-input 100)))))
 
 (deftest day11-2-test
   (testing "day11-2"
-    (is (= nil
-           (day11-2 input)))))
+    (is (= 685038186836 (day11 input 1000000)))))
