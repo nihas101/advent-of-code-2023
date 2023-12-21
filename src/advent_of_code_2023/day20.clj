69d0c624a9d16972c234e0161110e0de98e1a873
Nikita Hasert <nikitahasert@gmail.com>
2023-12-21 20:36:16 +0100

    Day 20: Solve part 1 and 2

--- src/advent_of_code_2023/day20.clj
+++ src/advent_of_code_2023/day20.clj
@@ -1,9 +1,100 @@
 (ns advent-of-code-2023.day20
   (:require
+   [advent-of-code-2023.utils.math :as m]
    [advent-of-code-2023.utils.string :as u]))
 
-(defn parse-input [input])
+(defn- parse-module [modules]
+  (let [[fun name & out] (re-seq #"[%&]|[^%&->\s]+" modules)]
+    (cond
+      (= "%" fun) {name {:out out :type :flip-flop :state false}}
+      (= "&" fun) {name {:out out :type :conjunction :state {}}}
+      (= "broadcaster" fun) {fun {:out (concat [name] out) :type :broadcast}})))
 
-(defn day20-1 [parsed-input])
+(defn parse-input [input]
+  (when input
+    (let [modules (reduce merge (mapv parse-module (u/split-sections input u/line-endings)))
+          conjunctions (mapv first (filterv (comp #{:conjunction} :type second) modules))]
+      (reduce (fn [mods [cnj inputs]]
+                (reduce (fn [m i] (assoc-in m [cnj :state i] false)) mods inputs))
+              modules
+              (mapv (fn [[cnj mods]]
+                      [cnj
+                       (mapv first
+                             (filterv (fn [[_ {:keys [out]}]] ((set out) cnj)) mods))])
+                    (mapv vector conjunctions (repeat modules)))))))
 
-(defn day20-2 [parsed-input])
+(defn- process-flip-flip [{:keys [state] :as flip-flop} pulse]
+  (when-not pulse
+    [(assoc flip-flop :state (not state))
+     (not state)]))
+
+(defn- process-conjunction [conjunction input pulse]
+  (let [{:keys [state] :as conjunction} (assoc-in conjunction [:state input] pulse)]
+    [conjunction (not (every? true? (vals state)))]))
+
+(defn- handle-signal [modules stack pulse-count goal]
+  (loop [modules modules
+         stack stack
+         pulse-count pulse-count]
+    (let [[source module-name pulse] (first stack)
+          s (pop stack)
+          {:keys [out type] :as module} (modules module-name)]
+      (cond
+        (and goal (not pulse) (= module-name goal)) [modules pulse-count true]
+        (empty? stack) [modules pulse-count false]
+
+        (= type :flip-flop) (if-let [[flip-flop new-pulse] (process-flip-flip module pulse)]
+                              (recur (assoc modules module-name flip-flop)
+                                     (reduce conj s (mapv vector (repeat module-name)
+                                                          out  (repeat new-pulse)))
+                                     (update pulse-count pulse inc))
+                              (recur modules s (update pulse-count pulse inc)))
+
+        (= type :conjunction) (let [[conjuction new-pulse] (process-conjunction module source pulse)]
+                                (recur (assoc modules module-name conjuction)
+                                       (reduce conj s (mapv vector (repeat module-name)
+                                                            out (repeat new-pulse)))
+                                       (update pulse-count pulse inc)))
+
+        (= type :broadcast) (recur modules
+                                   (reduce conj s (mapv vector (repeat module-name)
+                                                        out (repeat pulse)))
+                                   (update pulse-count pulse inc))
+
+        :else (recur modules s (update pulse-count pulse inc))))))
+
+(defn day20-1 [modules]
+  (let [limit 1000
+        stack (conj clojure.lang.PersistentQueue/EMPTY [:button "broadcaster" false])]
+    (loop [button-presses 0
+           modules modules
+           {^long high true
+            ^long low false
+            :as pulse-count} {true 0 false 0}]
+      (if (< button-presses limit)
+        (let [[new-modules pulse-count] (handle-signal modules stack pulse-count nil)]
+          (recur (inc button-presses) new-modules pulse-count))
+        (* high low)))))
+
+(defn- module-inputs [modules module-name]
+  (transduce
+   (comp
+    (filter (comp (fn [o] (some (set o) module-name)) :out second))
+    (map first))
+   conj [] modules))
+
+(defn- goal->button-presses [modules stack goal]
+  (loop [button-presses 1
+         modules modules]
+    (let [[new-modules _ goal-reached] (handle-signal modules stack {true 0 false 0} goal)]
+      (if goal-reached
+        button-presses
+        (recur (inc button-presses) new-modules)))))
+
+(defn day20-2 [modules goal]
+  (let [goal-conjunction (module-inputs modules [goal])
+        goal-conjunction-inputs (module-inputs modules goal-conjunction)
+        stack (conj clojure.lang.PersistentQueue/EMPTY [:button "broadcaster" false])]
+    (transduce
+     (map (fn [gci] (goal->button-presses modules stack gci)))
+     m/lcm goal-conjunction-inputs)))
--- test/advent_of_code_2023/day20_test.clj
+++ test/advent_of_code_2023/day20_test.clj
@@ -4,26 +4,36 @@
    [advent-of-code-2023.day20 :refer :all]
    [advent-of-code-2023.test-utils :as tu]))
 
-(defonce ^:private example-input (parse-input ""))
+(defonce ^:private example-input (parse-input "broadcaster -> a, b, c
+%a -> b
+%b -> c
+%c -> inv
+&inv -> a"))
+
+(defonce ^:private example-input-2 (parse-input "broadcaster -> a
+%a -> inv, con
+&inv -> b
+%b -> con
+&con -> output"))
 
 (def ^:private input (parse-input (tu/slurp-input "resources/day20.txt")))
 
 (deftest day20-1-example-test
   (testing "day20-1 example"
-    (is (= nil
-           (day20-1 example-input)))))
+    (is (= 32000000 (day20-1 example-input)))))
+
+(deftest day20-1-example-2-test
+  (testing "day20-1 example 2"
+    (is (= 11687500 (day20-1 example-input-2)))))
 
 (deftest day20-1-test
   (testing "day20-1"
-    (is (= nil
-           (day20-1 input)))))
+    (is (= 825896364 (day20-1 input)))))
 
 (deftest day20-2-example-test
-  (testing "day20-2 example"
-    (is (= nil
-           (day20-2 example-input)))))
+  (testing "day20-2 example 2"
+    (is (= 1 (day20-2 example-input-2 "output")))))
 
 (deftest day20-2-test
   (testing "day20-2"
-    (is (= nil
-           (day20-2 input)))))
+    (is (= 243566897206981 (day20-2 input "rx")))))
